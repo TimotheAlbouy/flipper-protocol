5eb938364bb2eb162c40d86641637d37587e99ac
TimotheAlbouy <38692015+TimotheAlbouy@users.noreply.github.com>
2020-04-20 12:41:19 +0200

    fix flpr

--- impl/flpr.py
+++ impl/flpr.py
@@ -10,7 +10,7 @@ class FLPR(Packet):
     name = "FLPR"
     fields_desc = [
         ShortField("id", random.getrandbits(16)),
-        FieldLenField("ctr", 0, count_of="hist"),
+        FieldLenField("ctr", None, count_of="hist"),
         ByteField("lim", random.getrandbits(8)),
         FieldListField("hist", None, IPField("", "0.0.0.0"), count_from=lambda pkt: pkt.ctr)
     ]
