f4d8e29a6ce84c64fbb583a44c1424422dd56e3d
TimotheAlbouy <38692015+TimotheAlbouy@users.noreply.github.com>
2020-04-21 05:13:15 +0200

    fix ips

--- impl/ips_flpr_1.py
+++ impl/ips_flpr_1.py
@@ -4,33 +4,20 @@ from flpr import FLPR, FLPR_PORT
 from ifaces import ifaces
 from util import ban_ip
 
-ball_histories = {}
 
-
-# verify that the new history is adding just 1 IP address from the old history
+# verify that the same IP does not appear 2 times in a row in the history
 def ips_flpr_1(pkt):
-    global ball_histories
     ip = pkt[IP]
     flpr = pkt[FLPR]
     print("flipper message received, ID = %s, CTR = %s, LIM = %s" % (flpr.id, flpr.ctr, flpr.lim))
-    if flpr.id not in ball_histories:
-        print("new ball")
-        if len(flpr.hist) > 1:
+    i = 0
+    while i < len(flpr.hist) - 1:
+        if flpr.hist[i] == flpr.hist[i+1]:
             print("ATTACK DETECTED: new ball with several IP in history")
+            print()
             ban_ip(ip.src)
-        else:
-            ball_histories[flpr.id] = flpr.hist
-            print("new ball history saved")
-            print("message forwarded")
-    else:
-        print("existing ball")
-        if ball_histories[flpr.id] != flpr.hist[:-1]:
-            print("ATTACK DETECTED: new history not based on previous one")
-            ban_ip(ip.src)
-        else:
-            ball_histories[flpr.id] = flpr.hist
-            print("ball history updated")
-            print("message forwarded")
+            return
+    print("message forwarded")
     print()
 
 
